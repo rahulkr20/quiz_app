beb6b200ee00eeb29f31e51062fc58e63e526cbc
manvi-kumar <kumarmanvi04@gmail.com>
2023-12-08 19:46:05 +0530

    jest test cases

--- frontend/src/components/Quiz.js
+++ frontend/src/components/Quiz.js
@@ -2,19 +2,34 @@ import React, { useState } from 'react';
 
 const questions = [
   {
-    question: 'Who is the prime minister of INDIA?',
-    options: ['Narendra Modi', 'Jawaharlal Nehru', 'Manmohan Singh', 'APJ Abdul kalam'],
-    correctAnswer: 'Narendra Modi',
+    question: 'Which of the following is a primary function of an Intrusion Detection System (IDS)?',
+    options: ['Data encryption', 'Network segmentation','Malware removal','Monitoring and analyzing network traffic'],
+    correctAnswer: 'Monitoring and analyzing network traffic',
   },
   {
-    question: 'If you freeze water, what do you get?',
-    options: ['Water Vapour', 'Ice', 'Fire', 'None of these'],
-    correctAnswer: 'Ice',
+    question: 'Which type of Intrusion Detection System (IDS) operates by comparing current system activity to a predefined profile of normal behavior?',
+    options: ['Signature-based','Anomaly-based','Hybrid-based','Behavior-based'],
+    correctAnswer: 'Anomaly-based',
   },
   {
-    question: ' How many planets are in our solar system?',
-    options: ['Nine', 'Five', 'Six', 'Eight'],
-    correctAnswer: 'Eight',
+    question: 'What does a Signature-based Intrusion Detection System (IDS) rely on for identifying malicious activities?',  
+    options: ['Statistical analysis','User behavior',' Known patterns or signatures','Anomaly detection'],
+    correctAnswer: 'Known patterns or signatures',
+  },
+  {
+    question: 'Which of the following is a limitation of Anomaly-based Intrusion Detection Systems (IDS)?',
+    options: ['High false positive rate','Slow response to new threats','Inability to detect known attacks',' Dependence on signature updates'],
+    correctAnswer: 'High false positive rate',
+  },
+  {
+    question: 'What is the role of a Network-based Intrusion Detection System (NIDS) in a cybersecurity infrastructure?',
+    options: [' Protecting endpoints','Monitoring network traffic','Scanning for malware','Encrypting data'],
+    correctAnswer: 'Monitoring network traffic',
+  },
+  {
+    question: 'Which type of Intrusion Detection System (IDS) is known for its ability to detect previously unknown threats?',
+    options: ['Signature-based','Anomaly-based','Heuristic-based','Behavior-based'],
+    correctAnswer: 'Heuristic-based',
   },
   
 
